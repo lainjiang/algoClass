fbe2ecc886ce4b0ac7e2259d89c725cb76f3e4f0
Lai 'Lain' Jiang <lain.lai.jiang@gmail.com>
2015-11-17 15:42:02 -0600

    Complete function to return fibonacci sequence

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -8,3 +8,28 @@ output   0 1 1 2 3 5 8 13 21 34 ...
 
 What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
 */
+
+// recursive solution w/o memoization
+var nthFibonacci = function(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+};
+
+// console.log(nthFibonacci(50)); // finished in 193.9s
+
+// recursive solution w memoization
+var memo = [];
+var test = 'test';
+var nthFibonacci = function(n) {
+  if (n <= 1) {
+    return n;
+  }
+  if (!memo[n]) {
+    memo[n] = nthFibonacci(n - 1) + nthFibonacci(n - 2);
+  }
+  return memo[n];
+};
+
+// console.log(nthFibonacci(50)); // finished in 0.1s
